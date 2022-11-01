d15187886e085c0ba96d83510f3cbf1caf305af0
Tejaswi-Chaudhari <tejschaudhari522002@gmail.com>
2022-11-01 17:51:08 +0530

    Header Styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,18 +4,18 @@ import {Container, Nav, Navbar, NavDropdown} from 'react-bootstrap'
 
 const Header = () => {
   return (
-    <Navbar className="header">
+    <Navbar className="header" collapseOnSelect expand="md">
       <Container>
-        <Navbar.Brand href="/" className="header-brand-txt">FarMagic</Navbar.Brand>
+        <Navbar.Brand href="/" className="header-brand-txt">Farmagic</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="header-nav-links">
             <Nav.Link href="/" className="header-txt">Home</Nav.Link>
             <Nav.Link href="/" className="header-txt">About Us</Nav.Link>
             <NavDropdown title="Products" id="basic-nav-dropdown" className="header-txt">
-              <NavDropdown.Item href="#action/3.1">Desi Ghee</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Oil</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Black Rice</NavDropdown.Item>
+              <NavDropdown.Item href="/">Desi Ghee</NavDropdown.Item>
+              <NavDropdown.Item href="/">Oil</NavDropdown.Item>
+              <NavDropdown.Item href="/">Black Rice</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
